c75100830e54d4f6ce0785ea41ba1bb8b37cfeb6
James Witnauer <jwitnaue@brockport.edu>
2026-06-02 19:20:02 +0000

    version 0.1.6

---
+++ R/SleepData.R
@@ -0,0 +1,21 @@
+#' Sleep Duration Data
+#'
+#' A small instructional dataset containing sleep duration and
+#' demographic information. The dataset is modeled after research on
+#' subjective temporalities during COVID-19.
+#'
+#' @format A data frame with 26 rows and 5 variables:
+#' \describe{
+#'   \item{Participant_ID}{Participant identifier.}
+#'   \item{Sleep_Duration}{Sleep duration.}
+#'   \item{Sex}{Participant sex.}
+#'   \item{Age}{Participant age in years.}
+#'   \item{Country}{Participant country.}
+#' }
+#'
+#' @source Dataset adapted for instructional use and modeled after
+#' Chaumon and colleagues (2022). The Blursday database as a resource
+#' to study subjective temporalities during COVID-19.
+#' Nature Human Behaviour, 6(11), 1587-1599.
+#' \doi{10.1038/s41562-022-01419-2}
+"SleepData"
