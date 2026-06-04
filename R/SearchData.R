b14f6457e54406d942f4bdb45dc59305e83bfc2c
James Witnauer <jwitnaue@brockport.edu>
2026-06-04 17:00:02 +0000

    version 0.1.7

---
+++ R/SearchData.R
@@ -0,0 +1,20 @@
+#' Interactive Search Data
+#'
+#' An instructional dataset for examples and exercises involving visual
+#' search or interactive search behavior. The dataset is modeled after
+#' research on visual search in a real-world interactive search paradigm.
+#'
+#' @format A data frame with 20 rows and 4 variables:
+#' \describe{
+#'   \item{Participant}{Description of Participant.}
+#'   \item{Easy}{Description of Easy.}
+#'   \item{Medium}{Description of Medium.}
+#'   \item{Hard}{Description of Hard.}
+#' }
+#'
+#' @source Dataset adapted for instructional use and modeled after
+#' Sauter, M., Stefani, M., and Mack, W. (2020). Towards interactive
+#' search: Investigating visual search in a novel real-world paradigm.
+#' Brain Sciences, 10(12), 927.
+#' \doi{10.3390/brainsci10120927}
+"SearchData"
